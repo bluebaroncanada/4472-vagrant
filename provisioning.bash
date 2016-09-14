a7c396f1109042edec5a012c7772d91c41127272
james <jbaron@digitalboundary.net>
2016-09-14 16:13:29 -0400

    Almost done

--- provisioning.bash
+++ provisioning.bash
@@ -19,29 +19,41 @@ apt-get -y install git
 apt-get -y install build-essential
 apt-get -y install libmysqlclient-dev
 
-su vagrant
+mkdir -p /var/www/tracks
+chown vagrant:vagrant /var/www/tracks
+chown root:vagrant /var/www
+chmod 770 /var/www/tracks
+chmod 770 /var/www
+
+exec sudo -i -u vagrant /bin/bash - << EOF
 
 cd ~
 wget -O ruby-install-0.6.0.tar.gz https://github.com/postmodern/ruby-install/archive/v0.6.0.tar.gz
 tar -xzvf ruby-install-0.6.0.tar.gz
 cd ruby-install-0.6.0/
-make install
+sudo make install
 
 #This takes a long time
-ruby-install --system ruby 2.3.1 -- --disable-install-doc
+sudo ruby-install ruby 2.3.1 -- --disable-install-doc
 
 cd ~
 wget -O chruby-0.3.9.tar.gz https://github.com/postmodern/chruby/archive/v0.3.9.tar.gz
 tar -xzvf chruby-0.3.9.tar.gz
 cd chruby-0.3.9/
-make install
+sudo make install
 
 
 echo source /usr/local/share/chruby/chruby.sh >> /home/vagrant/.bashrc
 echo source /usr/local/share/chruby/auto.sh >> /home/vagrant/.bashrc
+echo chruby 2.3.1 >> /home/vagrant/.bashrc
 
-chmod a+xr /usr/local/share/chruby/chruby.sh
-chmod a+xr /usr/local/share/chruby/auto.sh
+sudo chmod a+xr /usr/local/share/chruby/chruby.sh
+sudo chmod a+xr /usr/local/share/chruby/auto.sh
+
+source /usr/local/share/chruby/chruby.sh
+source /usr/local/share/chruby/auto.sh
+
+chruby 2.3.1
 
 gem install rails -v 4.2.5 --no-rdoc --no-ri
 
@@ -52,18 +64,9 @@ mysql -u root -ppassword -e 'CREATE DATABASE tracks;'
 gem install RedCloth -v '4.2.9'
 gem install nokogiri -v '1.6.7.2'
 
-mkdir -p /var/www/tracks
-chown vagrant:vagrant /var/www/tracks
-chown root:vagrant /var/www
-chmod 770 /var/www/tracks
-chmod 770 /var/www
-
 cd /var/www/tracks
 echo Cloning tracks ...
-git --quiet clone https://github.com/TracksApp/tracks.git /var/www/tracks
-
-
-
+git clone -q https://github.com/TracksApp/tracks.git /var/www/tracks
 
 
 cp /var/www/tracks/config/site.yml.tmpl /var/www/tracks/config/site.yml
@@ -71,12 +74,13 @@ cp /var/www/tracks/config/database.yml.tmpl /var/www/tracks/config/database.yml
 sed --in-place 's/  password.*/  password: password/' /var/www/tracks/config/database.yml
 sed --in-place 's/time_zone.*/time_zone\: \"Eastern Time \(US \& Canada\)\"/' /var/www/tracks/config/site.yml
 sed --in-place 's/secret_token.*/secret_token\: \"itsasecret\"/' /var/www/tracks/config/site.yml
+sed --in-place 's/# serve_static_assets.*/serve_static_assets: true/' config/site.yml
 
 bundle install --without development test sqlite
 bundle exec rake db:migrate RAILS_ENV=production
 
 bundle exec rake assets:precompile RAILS_ENV=production
-
+EOF
 
 
 exit 0
